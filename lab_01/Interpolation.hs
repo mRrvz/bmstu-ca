fd26f3d19154873a20448338091140aa0ae5f157
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-01-20 03:16:11 +0300

    finish interpolation

--- lab_01/Interpolation.hs
+++ lab_01/Interpolation.hs
@@ -1,6 +1,9 @@
 module Interpolation (
-    takeApproximation
+    takeApproximation,
+    createTable,
+    interpolation,
 ) where
+
 import Data.List
 import Data.Maybe
 
@@ -9,10 +12,22 @@ slice table n pos = take n $ drop pos table
 
 takeApproximation :: [(Double, Double)] -> Double -> Int -> [(Double, Double)]
 takeApproximation table x0 n
-    | (<) x0 . fst $ head table = take n table
-    | (>) x0 . fst $ last table = take n $ reverse table
+    | (<=) x0 . fst $ head table = take n table
+    | (>=) x0 . fst $ last table = reverse $ take n $ reverse table
     | otherwise = left ++ right
         where
-            index = (fromJust $ findIndex (\x -> fst x >= x0) table)
-            left = slice table (round $ n / 2) index
-            right = slice table (n - length left) (index - n - length left)
+            indexL = fromJust $ findIndex (\x -> fst x >= x0) table
+            left = slice table (n `div` 2 ) (indexL - n `div` 2)
+            indexR = fromJust $ findIndex (== last left) table
+            right = slice table (n - length left) (indexR + 1)
+
+createTable :: [Double] -> [Double] -> Int -> [[Double]]
+createTable _ (_:[]) _ = []
+createTable xs ys step =  (sepDifference xs ys step) : createTable xs (sepDifference xs ys step) (step + 1)
+    where
+        sepDifference _ (_:[]) _ = []
+        sepDifference xs ys step = (((ys !! 1) - (ys !! 0)) / ((xs !! (1 + step)) - (xs !! 0))) : sepDifference (tail xs) (tail ys) step
+
+interpolation :: [Double] -> [[Double]] -> Double -> Double
+interpolation xs [] x = 0
+interpolation xs table x = ((head $ head table) + interpolation (tail xs) (tail table) x) * (x - head xs)
--- lab_01/Main.hs
+++ lab_01/Main.hs
@@ -1,11 +1,15 @@
 import System.IO
 import Interpolation
+import Data.List
 
 main :: IO ()
 main = do
-    let xs = [1..20]
-    let ys = [x * x | x <- xs]
-    let table = zip xs ys
-    let x0 = 6.5
-    let n = 4
-    print $ takeApproximation table x0 n
+    putStrLn "Результат: "
+    print $ (interpolation (fst approx) table 1.5) + (head $ snd approx)
+    putStrLn "Таблица: \n| x | y | Разделенные разности |"
+    mapM_ print (transpose $ fst approx : snd approx : table)
+        where
+            xs = [1..20]
+            ys = [x * x | x <- xs]
+            approx = unzip $ takeApproximation (zip xs ys) 1.5 7
+            table = createTable (fst approx) (snd approx) 0
