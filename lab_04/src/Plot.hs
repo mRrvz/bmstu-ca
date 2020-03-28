8ecb74860384bc738b4d33b11e63ded7aae9257f
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-03-29 00:12:40 +0300

    Upload latex

--- lab_04/src/Plot.hs
+++ lab_04/src/Plot.hs
@@ -9,7 +9,7 @@ signal :: (Double -> Double) -> [Double] -> [(Double,Double)]
 signal f xs = [ (x, f x) | x <- xs ]
 
 plotApproximation f pts = toFile def "res.png" $ do
-    layout_title .= "Среднеквадратическое приближение"
+    layout_title .= "rms approximation"
     setColors [opaque blue, opaque red]
     plot (line "polynom" [signal f [-1,(-0.9)..8]])
     plot (points "points" pts)
