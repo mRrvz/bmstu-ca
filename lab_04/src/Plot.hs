6a764671bb10f8056a2caf48113086baca4b0b02
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-03-29 01:00:28 +0300

    Update docs

--- lab_04/src/Plot.hs
+++ lab_04/src/Plot.hs
@@ -8,8 +8,8 @@ import Graphics.Rendering.Chart.Backend.Cairo
 signal :: (Double -> Double) -> [Double] -> [(Double,Double)]
 signal f xs = [ (x, f x) | x <- xs ]
 
-plotApproximation f pts = toFile def "res.png" $ do
-    layout_title .= "rms approximation"
+plotApproximation f pts = toFile def "n6w1.png" $ do
+    layout_title .= "n = 6"
     setColors [opaque blue, opaque red]
     plot (line "polynom" [signal f [-1,(-0.9)..8]])
     plot (points "points" pts)
--- lab_04/src/TableGen.hs
+++ lab_04/src/TableGen.hs
@@ -3,5 +3,5 @@ fromPairs pair = (show $ fst pair) ++ " " ++ (show $ snd pair) ++ "\n"
 
 main :: IO ()
 main = writeFile "table.csv" $ foldl1 (++) $ map fromPairs $ zip xs $ map f xs
-    where xs = [1..20]
+    where xs = [1..15]
           f x = x * x
