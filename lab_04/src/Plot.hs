197ea57ea9bd04b63089348cfb6334f803af9bf5
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-03-29 19:59:59 +0300

    FINISH: lab04

--- lab_04/src/Plot.hs
+++ lab_04/src/Plot.hs
@@ -8,8 +8,8 @@ import Graphics.Rendering.Chart.Backend.Cairo
 signal :: (Double -> Double) -> [Double] -> [(Double,Double)]
 signal f xs = [ (x, f x) | x <- xs ]
 
-plotApproximation f pts = toFile def "n6w1.png" $ do
-    layout_title .= "n = 6"
+plotApproximation f pts = toFile def "test.png" $ do
+    layout_title .= "n = 9"
     setColors [opaque blue, opaque red]
-    plot (line "polynom" [signal f [-1,(-0.9)..8]])
+    plot (line "polynom" [signal f [-1,(-0.9)..11]])
     plot (points "points" pts)
