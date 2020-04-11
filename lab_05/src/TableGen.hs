e5ddcd63459133541d4804e735c3b9ed44fe9494
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-04-11 15:18:42 +0300

    fix lab05 files

--- lab_05/src/TableGen.hs
+++
@@ -1,7 +0,0 @@
-fromPairs :: (Double, Double) -> String
-fromPairs pair = (show $ fst pair) ++ " " ++ (show $ snd pair) ++ "\n"
-
-main :: IO ()
-main = writeFile "table.csv" $ foldl1 (++) $ map fromPairs $ zip xs $ map f xs
-    where xs = [1..15]
-          f x = x * x
