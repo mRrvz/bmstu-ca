1f3c953e0ee21af2b4bfd5a779f0af3ac7e44934
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2020-05-05 00:37:52 +0300

    Fix some issues

--- lab_05/src/Main.hs
+++ lab_05/src/Main.hs
@@ -2,14 +2,12 @@ module Main where
 
 import System.IO
 import Integration
+import Plot
 
 main :: IO ()
 main = do
     tau <- putStrLn "Enter tau: " >> fmap (\x -> read x :: Double) getLine
     n <- putStrLn "Enter N: " >> fmap (\x -> read x :: Int) getLine
     m <- putStrLn "Enter M: " >> fmap (\x -> read x :: Int) getLine
-    mapM_ print [n, m]
     print $ simpson2 n m tau limits
     print $ gauss2 n m tau limits
-    --print $ gauss m 0 (pi/2)
-    --print $ getCoeffs k
