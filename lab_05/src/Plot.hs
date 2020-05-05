a8812b56e7b48c824633a4a7bc64c8f3aa57dccd
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-05-05 03:42:30 +0300

    fix conflict

--- lab_05/src/Plot.hs
+++ lab_05/src/Plot.hs
@@ -1,15 +1,23 @@
-module Plot (
-    plotApproximation
-) where
 
+import System.IO
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
 signal :: (Double -> Double) -> [Double] -> [(Double,Double)]
 signal f xs = [ (x, f x) | x <- xs ]
 
-plotApproximation f pts = toFile def "test.png" $ do
-    layout_title .= "n = 9"
+plotApproximation table = toFile def "graph.png" $ do
+    layout_title .= "N = M = 5"
     setColors [opaque blue, opaque red]
-    plot (line "polynom" [signal f [-1,(-0.9)..11]])
-    plot (points "points" pts)
+    plot (line "ε (τ)" [table])
+    --plot (points "points" pts)
+
+parseTable :: [String] -> [(Double, Double)]
+parseTable table = foldr (\x acc -> (read (head x) :: Double, read (last x) :: Double) : acc ) [] words'
+    where words' = map words table
+
+main :: IO ()
+main = do
+    table <- openFile "table.csv" ReadMode >>= hGetContents >>= return . parseTable . lines
+    plotApproximation table
+    print table
