a3633d9cbb867b5d83aa3c677d6f0b5c474185dd
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2020-05-09 03:22:08 +0300

    FINISH: lab06

--- lab_06/src/Differentation.hs
+++ lab_06/src/Differentation.hs
@@ -1,13 +1,10 @@
 module Differentation (
     leftSide,
-    rightSide,
     centerDiff,
     rungeCenter,
     alignment,
     differential2
 ) where
-
-import Debug.Trace
 
 leftValue :: Double -> Double -> Double -> Double
 leftValue ly y h = (ly - y) / h
@@ -15,23 +12,14 @@ leftValue ly y h = (ly - y) / h
 leftSide :: [Double] -> Double -> [Double]
 leftSide ys h = map (\y -> (fst y - snd y) / h) $ zip (tail ys) ys
 
-rightSide :: [Double] -> Double -> [Double]
-rightSide ys h = map (\y -> (fst y - snd y) / h) $ zip (tail ys) ys
-
 centerDiff :: [Double] -> Double -> [Double]
 centerDiff ys h = map (\y -> (fst y - snd y) / (2 * h)) $ zip (tail $ tail ys) ys
-
-trace2 :: Show a => [Char] -> a -> a -> a
-trace2 name x y = trace (name ++ ": " ++ show x) y
 
 rungeCenter :: [Double] -> Double -> Int -> [Double]
 rungeCenter ys h p = runge
     where yh = tail $ leftSide ys h
           ymh = centerDiff ys h
           runge = map (\y -> fst y + (fst y - snd y) / (2^p - 1)) $ zip yh ymh
-
-getDiff :: [Double] -> Int -> Double
-getDiff a ind = a !! (ind + 1) - a !! ind
 
 fst' :: (Double, Double, Double) -> Double
 fst' (x, _, _ ) = x
@@ -53,10 +41,5 @@ alignment ys xs h = a
           phi = map (\x -> -1 / x) xs
           a = map (
             \i ->
-                ((ys !! i) ^ 2 / (xs !! i) ^2) *
+                ((ys !! i) ^2 / (xs !! i) ^2) *
                     leftValue (ksi !! (i + 1)) (ksi !! i) (phi !! (i + 1) - phi !! i)) [0..length ys - 2]
-
-          --f = leftSide (map (\y -> -1/y) ys) h
-          --a = map (
-          -- \y ->
-                --((fst' y)^2) / ((snd' y)^2) * thd' y) $ zip3 ys xs f
--- lab_06/src/Main.hs
+++ lab_06/src/Main.hs
@@ -1,19 +1,16 @@
 import System.IO
 import Differentation
 
+f :: [Double]
+f = [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]
+
+x :: [Double]
+x = [1..6]
+
 main :: IO ()
 main = do
-    print $ leftSide [0.571, 0.889, 1.091, 1.231, 1.333, 1.412] 1
-    print $ rightSide [0.571, 0.889, 1.091, 1.231, 1.333, 1.412] 1
-    print $ "center:"
-    print $ centerDiff [0.571, 0.889, 1.091, 1.231, 1.333, 1.412] 1
-    print $ "runge:"
-    print $ rungeCenter [0.571, 0.889, 1.091, 1.231, 1.333, 1.412] 1 1
-    --print $ alignment [1..6] [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]
-    --print $ alignment [1..6] $ map (\x -> x^3) [1..6]
-    print "second:"
-    print $ differential2 [0.571, 0.889, 1.091, 1.231, 1.333, 1.412] 1
-    print "flex entered: "
-    print $ alignment [0.571, 0.889, 1.091, 1.231, 1.333, 1.412] [1..6] 1
-    print $ alignment (map (\x -> x / (1 + x)) [1..6]) [1..6] 1
-    --print $ (map (\x -> x / (1 + x)) [1..6])
+    putStr "Left side: " >> print (leftSide f 1)
+    putStr "Center:" >> print (centerDiff f 1)
+    putStr "Runge: " >> print (rungeCenter f 1 1)
+    putStr "Alignment variables: " >> print (alignment f x 1)
+    putStr "Second derivative: " >> print (differential2 f 1)
